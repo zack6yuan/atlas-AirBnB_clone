9df0cd1fc9720922cfd4729453cacbe2609caa03
zack6yuan <zack6yuan@gmail.com>
2024-09-26 12:32:00 -0500

    started basemodel

---
+++ models/base_model.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Module for "BaseModel """
+
+
+class BaseModel:
+    """ Method: create "BaseModel" class """
--- tests/test_basemodel.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-""" Module for "BaseModel """
-
-
-class BaseModel:
-    """ Method: create "BaseModel" class """
