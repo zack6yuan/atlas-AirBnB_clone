82e623d48ee72f760c93a76f60b2d889017ac9b2
zack6yuan <zack6yuan@gmail.com>
2024-09-29 19:11:14 +0000

    started console

---
+++ console.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module for the console of the AirBnB clone """
+import cmd
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import models
+from models.stotage import storage
+
+class HBNBCommand(cmd.Cmd):
+    """ Method: create class for HBNB console """
+    
+
