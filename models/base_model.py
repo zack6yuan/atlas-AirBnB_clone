0c3501c54979e0ba998ab6544d0b32b568be33ba
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-28 21:40:47 -0500

    added code for base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,58 @@
 #!/usr/bin/python3
-""" Module for "BaseModel """
+"""
+Base model that provides common attributes and methods
+for other models to inherit.
+"""
+import uuid
+from datetime import datetime
+import models
 
 
 class BaseModel:
-    """ Method: create "BaseModel" class """
+    """
+    Defines the BaseModel class with attributes and methods
+    that provide structure for other classes.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """ Initializes a new instance of BaseModel.
+        If no kwargs are provided, initializes a new instance with
+        unique ID and current timestamp. If kwargs are provided,
+        initializes from the kwargs."""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            for key, val in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, val)
+
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel instance.
+
+        Format: [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the 'updated_at' timestamp and saves the instance
+        to storage via models.storage.save().
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """ Converts the instance to a dictionary,
+        including the class name and timestamp fields."""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        formatTime = "%Y-%m-%dT%H:%M:%S.%f"
+        new_dict["created_at"] = self.created_at.strftime(formatTime)
+        new_dict["updated_at"] = self.updated_at.strftime(formatTime)
+        return new_dict
