0df8f7eb4b4c5786f361d6142b2576e53291ef33
zack6yuan <zack6yuan@gmail.com>
2024-09-29 19:55:48 +0000

    defined functions and placed temporary pass

--- console.py
+++ console.py
@@ -26,6 +26,32 @@ class HBNBCommand(cmd.Cmd):
         """ Method: empty line doesn't affect anything """
         pass
 
+    def do_create(self, arg):
+        """ Method: creates new istance of BaseModel,
+        saves it to the JSON file, and prints the id """
+        pass
+    
+    def do_show(self, arg):
+        """ Method: prints the string representation of an
+        instance based on the class name and the id """
+        pass
+
+    def do_destroy(self, arg):
+        """ Method: deletes an instance based on the class
+        name and the id, and saves change to JSON file """
+        pass
+
+    def do_all(self, arg):
+        """ Method: prints all string representation of all 
+        instances based or not on the class name """
+        pass
+
+    def do_update(self, arg):
+        """ Method: updates an instance based on the class
+        name and id by adding or updating attribute, and saves
+        change to JSON file """
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
