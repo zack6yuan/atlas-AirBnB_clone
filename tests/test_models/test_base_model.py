a0a5a2669b1dab788d418ee3df80d1546b2b81d2
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-30 18:26:45 -0500

    added test cases for base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 
 import unittest
 from models.base_model import BaseModel
+from models.user import User
 from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
@@ -17,3 +18,19 @@ class TestBaseModel(unittest.TestCase):
         self.assertIs(type(my_model.updated_at), datetime)
         self.assertIs(type(my_model.to_dict()), dict)
         self.assertIs(type(my_model.__str__()), str)
+
+    def test_save(self):
+        """Test save method"""
+        my_model = BaseModel()
+        updated_at = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(updated_at, my_model.updated_at)
+
+    def test_to_dict(self):
+        """Test to_dict method"""
+        my_model = BaseModel()
+        dict_model = my_model.to_dict()
+        self.assertIn('__class__', dict_model)
+        self.assertIn('id', dict_model)
+        self.assertIn('created_at', dict_model)
+        self.assertIn('updated_at', dict_model)
