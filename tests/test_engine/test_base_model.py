3c5bf9ad69def55970d0ab762faac4b32404ddf9
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-30 18:12:42 -0500

    added test base model

---
+++ tests/test_engine/test_base_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""unit test for base model class"""
+
+import unittest
+from models.base_model import BaseModel
+import datetime
+
+class TestBaseModel(unittest.TestCase):
+    """Test case for BaseModel"""
+
+    def test_normal_cases_base_model(self):
+        """Test normal cases"""
+        my_model = BaseModel()
+        self.assertIs(type(my_model), BaseModel)
+        self.assertIs(type(my_model.id), str)
+        self.assertIs(type(my_model.created_at), datetime)
+        self.assertIs(type(my_model.updated_at), datetime)
+        self.assertIs(type(my_model.to_dict()), dict)
+        self.assertIs(type(my_model.__str__()), str)
+        self.assertIs(type(my_model.save()), None)
