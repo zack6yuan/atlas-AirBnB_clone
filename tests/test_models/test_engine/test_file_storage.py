758d4eb236f6bd37cddb1541cea790b76b62d351
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-30 18:35:34 -0500

    testing checker for file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 """ unit test for base model class"""
 
+import os
+import pep8
+import json
+import models
 import unittest
+from datetime import datetime
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
-class TestBaseModel(unittest.TestCase):
-    """ Test cases for the BaseModel class """
+class TestFileStorage_instantiation(unittest.TestCase):
+    """ define setup and teardown """
 
-    def test_normal_cases_base_model(self):
-        """ Test normal cases """
-        my_model = FileStorage()
-        my_dict = my_model.all()
-        self.assertIs(type(my_dict), dict)
+    def setUp(self):
+        """ sets up the class """
+        self.my_model = FileStorage()
+
+    def tearDown(self):
+        """ tears down the class """
+        del self.my_model
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
