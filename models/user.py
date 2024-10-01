07e4db4fa15d2218a68b84a5f2879bf885eea688
zack6yuan <zack6yuan@gmail.com>
2024-10-01 18:01:38 +0000

    revert user.

--- models/user.py
+++ models/user.py
@@ -6,11 +6,12 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ Class User that inherits from BaseModel """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
     def __init__(self, email, password, first_name, last_name):
         """ Method to initialize the user instance """
         super().__init__()
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
     
