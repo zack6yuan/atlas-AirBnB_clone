b2bc896d1c1104df8398e36dfeb94b9e9e7ba6e8
zack6yuan <zack6yuan@gmail.com>
2024-10-01 18:03:49 +0000

    update city test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 """ City unittest """
 import unittest
-import models
-from models import City
 
 class TestCity(unittest.TestCase):
-    """ class defined to test City """
-    def setUp(self):
-        """ Set up for the tests """
-        self.city = City()
