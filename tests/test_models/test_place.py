e70fe0ccf73c34ed39cb1aab97c41121e840b25c
zack6yuan <zack6yuan@gmail.com>
2024-10-02 04:07:13 +0000

    update place test.

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -24,3 +24,6 @@ class TestPlace(unittest.TestCase):
         self.assertEqual(self.place.latitude, 0.0)
         self.assertEqual(self.place.longitude, 0.0)
         self.assertEqual(self.place.amenity_ids, [])
+        
+if __name__ == "__main__":
+    unittest.main()
