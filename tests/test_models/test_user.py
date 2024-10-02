1847e43c889901ee6a4a110d36b6640711e38297
zack6yuan <zack6yuan@gmail.com>
2024-10-02 04:41:19 +0000

    update user test.

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,3 +8,15 @@ from models.user import User
 
 class TestUser(unittest.TestCase):
     """Test case for User"""
+    self.user = User()
+
+def test_user_values(self):
+    """ Method: Start test for "user" """
+    self.user = User()
+    self.assertEqual(self.user.email, "")
+    self.assertEqual(self.user.password, "")
+    self.assertEqual(self.user.first_name, "")
+    self.assertEqual(self.user.last_name, "")
+
+if __name__ == "__main__":
+    unittest.main()
