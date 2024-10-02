7077ac4050734a4c64ed01c0c8cac13d5a3da278
zack6yuan <zack6yuan@gmail.com>
2024-10-02 04:33:05 +0000

    update city test.

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -6,4 +6,13 @@ class TestCity(unittest.TestCase):
     """ Test case for City """
     def setUp(self):
         """ Set up for the tests """
-        
+        self.city = City()
+
+    def test_city_values(self):
+        """ Method: Start test for "city" """
+        self.city = City()
+        self.assertEqual(self.city.state_id, "")
+        self.assertEqual(self.city.name, "")
+
+if __name__ == "__main__":
+    unittest.main()
