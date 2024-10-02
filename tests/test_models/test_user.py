cf038994ad670eb1dabd4063b1cb58281110f5e3
zack6yuan <zack6yuan@gmail.com>
2024-10-02 04:49:15 +0000

    update user test.

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,13 +10,25 @@ class TestUser(unittest.TestCase):
     """Test case for User"""
     self.user = User()
 
-def test_user_values(self):
-    """ Method: Start test for "user" """
-    self.user = User()
-    self.assertEqual(self.user.email, "")
-    self.assertEqual(self.user.password, "")
-    self.assertEqual(self.user.first_name, "")
-    self.assertEqual(self.user.last_name, "")
+    def test_user_setup(self):
+        """ Method: Start test for "user" """
+        self.user = User()
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+
+    def test_user_values(self):
+        """ Check that attributes were assigned correctly """
+        self.user.email = "john.appleseed@mail.com"
+        self.user.password = "Apple123"
+        self.user.first_name = "John"
+        self.user.last_name = "Appleseed"
+
+        self.assertEqual(self.user.email, "johnappleseed@mail.com")
+        self.assertEqual(self.user.password, "Apple123")
+        self.assertEqual(self.user.first_name, "John")
+        self.assertEqial(self.user.last_name, "Appleseed")
 
 if __name__ == "__main__":
     unittest.main()
