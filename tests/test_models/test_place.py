10074885826288295f84b14c412f04f9cfe3936f
zack6yuan <zack6yuan@gmail.com>
2024-10-02 03:58:01 +0000

    update place unittest

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
 """ unittest for place """
 import unittest
+from models.place import place
 
 class TestPlace(unittest.TestCase):
-    """ Test case for Place """
-    def setUp(self):
-        """ Set up for the tests """
-        
+    """ Class defined for "place" testcase """
+    def test_place_setup(self):
+        """ Method: represent place "testcase" """
+        self.place = Place()
+        if self.place in place:
+            return True
+
+    def test_place_values(self):
+        self.place = Place()
+        self.assertEqual(self.place.city_id, "")
+        self.assertEqual(self.place.user_id, "")
+        self.assertEqual(self.place.name, "")
+        self.assertEqual(self.place.description, "")
+        self.assertEqual(self.place.number_rooms, 0)
+        self.assertEqual(self.place.number_bathrooms, 0)
+        self.assertEqual(self.place.max_guest, 0)
+        self.assertEqual(self.place.price_by_night, 0)
+        self.assertEqual(self.place.user_id, "")
+        self.assertEqual(self.place.latitude, 0.0)
+        self.assertEqual(self.place.longitude, 0.0)
+        self.assertEqual(self.place.amenity_ids, [])
